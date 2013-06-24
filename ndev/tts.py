f23149a73a3132e450bf4957487ee86753f52fef
Tikku, Nirvana <nirvana.tikku@nuance.com>
2013-06-23 23:33:59 -0700

    Raise exception in TTS handler if bad file type

--- ndev/tts.py
+++ ndev/tts.py
@@ -518,14 +518,17 @@ class TTS(object):
 		if filename is None:
 			return 'wav' # assume wav
 		extension = filename[filename.rindex('.')+1:]
-		return {
+		ret = {
 			'wav': 'wav',
 			'ogg': 'speex',
 			'spx': 'speex',
 			'amr': 'amr',
 			'qcp': 'qcelp',
 			'evrc': 'evrc',
-		}[extension]
+		}
+		if extension not in ret:
+			raise Exception("Bad file extension: '%s' is not supported" % extension)
+		return ret[extension]
 	
 """
 TTS Response
